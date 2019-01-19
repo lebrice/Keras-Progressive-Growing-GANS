d89b0a1b061bc5453d2f16655c42a1c789cb17fe
Fabrice Normandin <fabrice.normandin@gmail.com>
2019-01-18 20:06:40 -0500

    Added the growing mechanism for the Discriminator
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- networks_keras.py
+++ networks_keras.py
@@ -1,4 +1,4 @@
-from typing import NamedTuple, Type, List, Tuple, Iterable
+from typing import NamedTuple, Type, List, Tuple, Iterable, Dict
 from functools import wraps
 
 import numpy as np
@@ -67,6 +67,11 @@ class WeightScaledConv2D(WeightScaled(tf.keras.layers.Conv2D)):
         super().__init__(
             filters=filters, kernel_size=kernel_size, activation=activation,
             strides=strides, data_format=data_format, padding=padding, *args, **kwargs)
+
+
+class Downscale2D(tf.keras.layers.AveragePooling2D):
+    def __init__(self, *args, **kwargs):
+        super().__init__(data_format="channels_first", *args, **kwargs)
 
 
 class Conv2DDownscale2D(WeightScaledConv2D):
@@ -368,41 +373,36 @@ class DiscriminatorBlock(tf.keras.models.Sequential):
         self.stage = stage_of_resolution(resolution)
         self.is_first_block: bool
         self.mixing_factor: float = 0.0
-
-    def build(self, input_shape: tf.TensorShape):
-        # We received the image as input. We are the first block.
-        self.is_first_block = input_shape[-3].value == 3
-        if self.is_first_block:
-            self.add(FromRGB(num_filters(self.stage), name=f"disc_{self.stage}_from_rgb"))
-        self.add(WeightScaledConv2D(filters=num_filters(self.stage), kernel_size=3, name=f"disc_{self.stage}_conv0"))
-        self.add(WeightScaledConv2D(filters=num_filters(self.stage+1), kernel_size=3, name=f"disc_{self.stage}_conv1"))
-        self.add(tf.keras.layers.AveragePooling2D(data_format="channels_first", name=f"disc_{self.stage}_avg_pooling"))
-        super().build(input_shape)
-
-    def call(self, inputs: tf.Tensor, training=False) -> tf.Tensor:
-        if self.is_first_block:
-            print("TODO: First block of the discriminator isn't implemented yet.")
-            exit()
-        else:
-            return super().call(inputs, training)
+        self.add(WeightScaledConv2D(filters=num_filters(self.stage+1), kernel_size=3, name=f"disc_{self.stage}_conv0"))
+        self.add(WeightScaledConv2D(filters=num_filters(self.stage), kernel_size=3, name=f"disc_{self.stage}_conv1"))
+        self.add(Downscale2D(name=f"disc_{self.stage}_avg_pooling"))
 
 
 class Discriminator(tf.keras.models.Sequential):
-    def __init__(self, *args, **kwargs):
-        self.resolution: int
+    def __init__(self, resolution: int = None, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.built = False
+
+        self.resolution: int = resolution
+        self.layers: List[tf.keras.layers.Layer]
+        self._mixing_factor: float = 0.0
+        if self.resolution is not None:
+            self.build(tf.TensorShape([3,self.resolution, self.resolution]))
 
     def build(self, input_shape: tf.TensorShape):
+        self.clear()
+
         self.resolution = input_shape[-1].value
         self.stage = stage_of_resolution(self.resolution)
 
         self.add(tf.keras.layers.InputLayer([3, self.resolution, self.resolution]))
-        self.add(FromRGB(filters_for(self.resolution), name="from_rgb"))       
+        self.add(FromRGB(num_filters(self.stage), name=f"disc_{self.stage}_from_rgb"))
+        
         for i in range(self.stage, 0, -1):
-            if i == 0:
-                pass
-            self.add(DiscriminatorBlock(resolution_of_stage(i)))   
+            self.add(DiscriminatorBlock(resolution_of_stage(i)))
+            from_rgb_layer_name = f"disc_{i}_from_rgb"
+            if not hasattr(self, from_rgb_layer_name):
+                from_rgb_layer = FromRGB(num_filters(self.stage), name=from_rgb_layer_name)
+                setattr(self, from_rgb_layer_name, from_rgb_layer)
 
         self.add(MinibatchStdDevLayer())
         self.add(WeightScaledConv2D(filters=num_filters(1), kernel_size=3, name="conv0"))
@@ -411,14 +411,61 @@ class Discriminator(tf.keras.models.Sequential):
         self.add(WeightScaledDense(units=1, gain=1, activation=tf.keras.activations.linear, name="dense0"))
         super().build(input_shape)
 
-    def call(self, inputs: tf.Tensor, training=False) -> tf.Tensor:
-        print("call was called.")
-        return super().call(inputs, training)
+    @property
+    def mixing_factor(self) -> tf.Tensor:
+        return tf.clip_by_value(self._mixing_factor, 0.0, 1.0)
     
-    def grow(self) -> None:
-        """Adds a layer at the front of the discriminator."""
-        pass
-        # current_layers = [l for l in self.layers]
+    @mixing_factor.setter
+    def mixing_factor(self, value) -> None:
+        self._mixing_factor = value
+
+    def call(self, inputs: tf.Tensor, training=False) -> tf.Tensor:
+        if self.stage == 0:
+            # we have a 4x4 input resolution. No need to mix anything.
+            # the output is just the chaining of the layers.
+            return super().call(inputs, training)
+        
+        downscaled_inputs = Downscale2D()(inputs)
+        from_rgb_previous = getattr(self, f"disc_{self.stage}_from_rgb")
+        downscaled_features = from_rgb_previous(downscaled_inputs)
+        
+        from_rgb_current = self.layers[0]
+        # also equivalent:
+        # from_rgb_current = getattr(self, f"disc_{self.stage}_from_rgb")
+        first_block = self.layers[1]
+
+        features = from_rgb_current(inputs)
+        first_block_output = first_block(features)
+        
+        x = (
+            (1-self.mixing_factor) * downscaled_features +
+            self.mixing_factor * first_block_output
+        )
+        print(x.shape)
+
+        for layer in self.layers[2:]:
+            x = layer(x)
+
+        return x
+    
+    def clear(self):
+        """Remove all layers. TODO: check if this is working."""
+        while len(self.layers) != 0:
+            self.pop()
+
+    def grow(self) -> "Discriminator":
+        """
+        Creates a bigger discriminator, copies the current weights over, and returns it.
+        """
+        new = Discriminator(self.resolution * 2)
+        current_layers = {
+            l.name: l for l in self.layers 
+        }
+        for layer in new.layers:
+            if layer.name in current_layers:
+                print(f"layer '{layer.name}' is common.")
+                layer.set_weights(current_layers[layer.name].get_weights())
+        return new
 
 def stage_of_resolution(res: int) -> int:
     """Measure of the stage of growth of the model.
@@ -435,26 +482,26 @@ def filters_for(resolution) -> int:
     res_log2 = log2(resolution)
     return num_filters(res_log2-1)
 
-# x = tf.random_normal([1, 3, 4, 4])
-# disc_1 = Discriminator()
-# y = disc_1(x)
-# disc_1.summary()
-# print(y.shape)
+x = tf.random_normal([1, 3, 8, 8])
+disc_1 = Discriminator()
+y = disc_1(x)
+disc_1.summary()
 
-# # trained_weights = discriminator.get_weights()
-# # trained_config = discriminator.get_config()
-# # print(trained_config)
-# # discriminator2 = Discriminator.from_config(discriminator.get_config(), custom_objects={
-# #     "FromRGB": FromRGB
-# # })
+x_2 = tf.random_normal([1,3,16,16])
+disc_2 = disc_1.grow()
+y_2 = disc_2(x_2)
+disc_2.summary()
+
+# trained_weights = discriminator.get_weights()
+# trained_config = discriminator.get_config()
+# print(trained_config)
+# discriminator2 = Discriminator.from_config(discriminator.get_config(), custom_objects={
+#     "FromRGB": FromRGB
+# })
 
 # x2 = tf.random_normal([1, 3, 1024, 1024])
 # disc_2 = Discriminator()
-# disc_2.set_weights(disc_1.get_weights)
+# # disc_2.set_weights(disc_1.get_weights)
 # y2 = disc_2(x2)
 # disc_2.summary()
 # print(y2.shape)
-# discriminator2.summary()
-# print(discriminator.compute_output_shape(x.shape))
-# discriminator.summary()
-# discriminator.layers[1].summary()
